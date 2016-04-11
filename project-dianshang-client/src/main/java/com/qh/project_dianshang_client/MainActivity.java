1689a05dd6d76bafdc82590ad1c3873b3da3cc97
qq245521957 <245521957@qq.com>
2016-04-11 20:56:26 +0800

    修改了mainactivity的layout

--- project-dianshang-client/src/main/java/com/qh/project_dianshang_client/MainActivity.java
+++ project-dianshang-client/src/main/java/com/qh/project_dianshang_client/MainActivity.java
@@ -1,13 +1,84 @@
 package com.qh.project_dianshang_client;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.dxjia.library.ImageTextButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /**
+     * 分类
+     */
+    ImageTextButton btn_classify;
+    /**
+     * 我的
+     */
+    ImageTextButton btn_mine;
+    /**
+     * 发现
+     */
+    ImageTextButton btn_discover;
+    /**
+     * 首页
+     */
+    ImageTextButton btn_index;
+
+    /**
+     * 主体
+     */
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        initListener();
+    }
+
+    /**
+     * 变量赋值
+     */
+    private void init() {
+        btn_classify=(ImageTextButton) findViewById(R.id.btn_classify);
+        btn_mine=(ImageTextButton) findViewById(R.id.btn_mine);
+        btn_discover=(ImageTextButton) findViewById(R.id.btn_discover);
+        btn_index=(ImageTextButton) findViewById(R.id.btn_index);
+
+    }
+
+    /**
+     * 为对象添加监听器
+     */
+    public void initListener(){
+        btn_mine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"mine",Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_mine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"mine",Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_mine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"mine",Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_mine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"mine",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
